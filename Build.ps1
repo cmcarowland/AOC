92a687c29f7164e98914fe17a036cd6310f553c5
rrowland <cmcarowland@live.com>
2023-11-29 11:09:30 -0500

    Updated Build Script
    refactor: Added /DEBUG flag to build to show line numbers for bugs.

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,3 @@
 param($script, $output)
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:$output $script
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:$output /DEBUG $script
