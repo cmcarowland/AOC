80ee46d1a5fbc71188723eaef1f0d82264a6d89d
rrowland <cmcarowland@live.com>
2023-11-30 12:37:51 -0500

    Created Day 8 project
    feat: Created 2022 Day 8 project.

---
+++ 2022/Day8/Day8.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        var lines = File.ReadAllLines(@"2022\Day8\" + "Input.txt");
+    }
+
+    public static int StarOne(string[] lines)
+    {
+        return -1;
+    }
+
+    public static int StarTwo(string[] lines)
+    {
+        return -1;
+    }
+}
