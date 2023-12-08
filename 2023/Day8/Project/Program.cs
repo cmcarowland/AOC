58b4281f0a92a2e334d424dde86a0f0eba5e2198
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2023-12-08 17:58:00 +0000

    Completed 2023 Day8 Star One
    feat: Added Day 8 project for 2023.
    feat: Completed first star challenge.

---
+++ 2023/Day8/Project/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Program
+{
+    static Dictionary<string, Tuple<string, string>> nodes = new Dictionary<string, Tuple<string, string>>();
+    static string directions = "";
+
+    static void Main(string[] argv)
+    {
+        var lines = File.ReadAllLines(argv[0]);
+        directions = lines[0].Trim();
+        foreach(var node in lines.Skip(2))
+        {
+            var split = node.Split(" = (");
+            var lr = split[1].Replace(")", "").Split(", ");
+            nodes.Add(split[0].Trim(), new Tuple<string, string>(lr[0], lr[1]));
+        }
+
+        // StarOne();
+        StarTwo();
+    }
+
+    static void StarOne()
+    {
+        var kvp = nodes.First(x => x.Key == "AAA");
+        var end = nodes.First(x => x.Key == "ZZZ");
+        
+        int count = 0;
+        while(kvp.Key != end.Key)
+        {
+            char nextMove = directions[count % directions.Length];
+            if(nextMove == 'L')
+            {
+                kvp = nodes.First(x => x.Key == kvp.Value.Item1);
+            }
+            else
+            {
+                kvp = nodes.First(x => x.Key == kvp.Value.Item2);
+            }
+
+            count ++;
+        }
+
+        Console.WriteLine(count);
+    }
+    
+    static void StarTwo()
+    {
+        var kvp1 = nodes.First(x => x.Key.EndsWith('A'));
+        var kvp2 = nodes.First(x => x.Key.EndsWith('A') && x.Key != kvp1.Key);
+
+        Console.WriteLine(kvp1);
+        Console.WriteLine(kvp2);
+        return;        
+        // int count = 0;
+        // while(kvp.Key != end.Key)
+        // {
+        //     char nextMove = directions[count % directions.Length];
+        //     if(nextMove == 'L')
+        //     {
+        //         kvp = nodes.First(x => x.Key == kvp.Value.Item1);
+        //     }
+        //     else
+        //     {
+        //         kvp = nodes.First(x => x.Key == kvp.Value.Item2);
+        //     }
+
+        //     count ++;
+        // }
+
+        // Console.WriteLine(count);
+    }
+}
--- CreateProject.py
+++ CreateProject.py
@@ -6,11 +6,11 @@ import subprocess
 year = int(sys.argv[1])
 day = int(sys.argv[2])
 url =  '/'.join(['https://adventofcode.com',str(year),'day',str(day),'input'])
-with open('Secret.txt', 'r') as iFile:
-    cookie = iFile.readline()
+#with open('Secret.txt', 'r') as iFile:
+    #cookie = iFile.readline()
 
-cookies = {'session':cookie}
-data = requests.get(url, cookies=cookies)       
+#cookies = {'session':cookie}
+#data = requests.get(url, cookies=cookies)       
 
 
 
@@ -19,5 +19,5 @@ print(projectFileName)
 subprocess.run(['dotnet', 'new', 'console', '-o', projectFileName])
 
 fileName = '/'.join([str(year), ('Day' + str(day)), 'Project', 'Input.txt'])
-with open(fileName, 'w') as oFile:
-    oFile.write(data._content.decode())
+#with open(fileName, 'w') as oFile:
+    #oFile.write(data._content.decode())
