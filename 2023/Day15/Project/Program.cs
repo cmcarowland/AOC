e273342bffa767dda86f4abe31156506f75cd756
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2023-12-15 12:59:39 +0000

    Created Day 15 project and completed Star One
    feat: Created Day 15 project.
    feat: Completed Star One Challenge.

---
+++ 2023/Day15/Project/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Versioning;
+using System.Data;
+
+
+class Program
+{
+    static string[] lines = new string[0];
+
+    static int Hash(string s)
+    {
+        int hash = 0;
+        foreach(var c in s)
+        {
+            hash += (int)c;
+            hash *= 17;
+            hash %= 256;
+        }
+
+        return hash;
+    }
+
+    static void Main(string[] argv)
+    {
+        lines = File.ReadAllLines(argv[0]);
+        int total = 0;
+        foreach(var config in lines[0].Split(','))
+            total += Hash(config);
+
+        Console.WriteLine(total);
+        StarOne();
+        //StarTwo();
+    }
+
+    static void StarOne()
+    {
+        
+    }
+
+    
+    static void StarTwo()
+    {
+       
+    }
+}
