b72929e9e9bde5070c4e09e5b7b750bde3007e63
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2023-12-28 13:32:24 +0000

    AOC 2021
    feat: Added Day1 project and completed star one.

---
+++ 2021/Day1/Project/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static string[] lines = new string[0];
+
+    public static void Main(string[] argv)
+    {
+        lines = File.ReadAllLines(argv[0]);
+        StarOne();
+        StarTwo();
+    }
+
+    public static void StarOne()
+    {
+        int count = 0;
+        var numbers = lines.Select(x => int.Parse(x)).ToArray();
+        for(int i = 1; i < numbers.Length; i++)
+        {
+            if(numbers[i - 1] < numbers[i])
+                count++;
+        }
+
+        Console.WriteLine("Increases : {0}", count);
+    }
+
+    public static void StarTwo()
+    {
+        
+    }
+}
+
