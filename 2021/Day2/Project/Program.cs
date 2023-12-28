ead4b7facfcece7890b7179d4a635cdf0d473646
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2023-12-28 14:38:14 +0000

    AOC 2021 Day 2
    feat: Added project for day 2.
    feat: Completed both star challenges.

---
+++ 2021/Day2/Project/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static string[] lines = new string[0];
+
+    public static void Main(string[] argv)
+    {
+        lines = File.ReadAllLines(argv[0]);
+        StarOne();
+        StarTwo();
+    }
+
+    public static void StarOne()
+    {
+        int depth = 0;
+        int forward = 0;
+
+        foreach (var line in lines)
+        {
+            var split = line.Split(' ');
+            if(split[0] == "forward")
+                forward += int.Parse(split[1]);
+            else
+            {
+                if(split[0] == "down")
+                    depth += int.Parse(split[1]);
+                else
+                    depth -= int.Parse(split[1]);
+            }
+        }
+
+        Console.WriteLine("Depth : {0} Forward : {1} Position {2}", depth, forward, depth * forward);
+    }
+
+    public static void StarTwo()
+    {
+        int depth = 0;
+        int forward = 0;
+        int aim = 0;
+
+        foreach (var line in lines)
+        {
+            var split = line.Split(' ');
+            var distance = int.Parse(split[1]);
+            if(split[0] == "forward")
+            {
+                forward += distance;
+                if(aim > 0)
+                    depth += aim * distance;
+            }
+            else
+            {
+                if(split[0] == "down")
+                    aim += distance;
+                else
+                    aim -= distance;
+            }
+        }
+
+        Console.WriteLine("Depth : {0} Forward : {1} Position {2:N0}", depth, forward, depth * forward);
+    }
+}
+
