51704c6e53d2d3052ad803390dbaeaad48d4d9c7
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-12-02 16:36:11 +0000

    Completed Day 2 Pt 1 2024
    feat: Implemented Day 2 2024 first star and submitted answer

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -1,3 +1,66 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please enter file path for data set");
+    }
+
+    pt1(&args[1]);
+    pt2(&args[1]);
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in fs::read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string());
+    }
+
+    result
+}
+
+fn is_safe(values: &Vec<i64>) -> bool {
+    let mut down : bool = false;
+    if values[0] > values[1] {
+        down = true;
+    }
+
+    for i in 0..values.len() - 1 {
+        //println!("{} {}", values[i], values[i + 1]);
+        let diff : i64 = (values[i] - values[i + 1]).try_into().unwrap();
+        //println!("Diff: {}", diff);
+
+        if down {
+            if diff <= 0 || diff > 3 {
+                return false
+            }
+        } else {
+            if diff >= 0 || diff < -3 {
+                return false
+            }
+        }
+    }
+
+    true
+}
+
+fn pt1(filename : &str) {
+    let lines = read_lines(filename);
+    let mut safe_lines : usize = 0;
+
+    for line in lines.iter() {
+        let parts : Vec<i64> = line.split(" ").map(|n| n.parse::<i64>().unwrap()).collect();
+        if is_safe(&parts) {
+            safe_lines += 1;
+            println!("Safe : {}", line);
+        }
+    }
+
+    println!("Safe Lines : {}", safe_lines);
+}
+
+fn pt2(filename : &str) {
+    let lines = read_lines(filename);
 }
