5092d8afecf9173a3eeff9e556bfcd7e6ea930e5
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-12-09 13:43:02 +0000

    Created Project for Day 5 2024
    feat: New project for Day 5

---
+++ 2024/day5/src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please enter file path for data set");
+    }
+
+    pt1(&args[1]);
+    pt2(&args[1]);
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in fs::read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string());
+    }
+
+    result
+}
+
+fn pt1(filename : &str) {
+    let _lines = read_lines(filename);
+}
+
+fn pt2(filename : &str) {
+    let _lines = read_lines(filename);
+}
