3cd47d8978af81f80a274c77891435f8322d099f
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-12-13 16:14:01 +0000

    Worked on Day7 2024
    chore: Refactoring all the code to try a different approach

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -33,8 +33,8 @@ fn pt1(filename : &str) -> i64 {
 
         while *bit_map.last().unwrap() == 0 {
             let mut operands : VecDeque<i64> = split[1].trim().split(' ').map(|x| x.parse::<i64>().unwrap()).collect();
-            let mut index = 1;
             let mut operation = 0;
+
             while operands.len() > 1 {
                 let first = operands.pop_front().unwrap();
                 
@@ -70,50 +70,106 @@ fn pt1(filename : &str) -> i64 {
     return answer;
 }
 
-fn pt2(filename : &str) -> i64 {
+fn concat(start : u64, addin : u64) -> u64 {
+    let mut base : u64 = 1;
+    while addin > base {
+        base *= 10;
+    }
+
+    // println!("{} || {} = {}", start, addin, start * base + addin);
+    return (start * base) + addin;
+}
+
+fn is_valid(start : u64, total : u64, operands : &VecDeque<u64>, mut index : usize) -> bool {
+    // println!("Start : {}", start);
+    if index == operands.len() {
+        // println!("End : {}", start);
+        if start == total {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    let other = operands[index];
+    index += 1;
+    return 
+        is_valid(start + other, total, operands, index) ||
+        is_valid(start * other, total, operands, index) ||
+        is_valid(concat(start, other), total, operands, index);
+}
+
+fn pt2(filename : &str) -> u64 {
     let lines = read_lines(filename);
-    let mut answer = 0;
+    let mut answer: u64 = 0;
 
     for line in lines {
         // println!("{}", line);
         let split : Vec<&str> = line.split(':').collect();
-        let total = split[0].parse::<i64>().unwrap();
-        let mut bit_map: Vec<i8> = vec![0; split[1].trim().split(' ').count()];
-
-        while *bit_map.last().unwrap() == 0 {
-            let mut operands : VecDeque<i64> = split[1].trim().split(' ').map(|x| x.parse::<i64>().unwrap()).collect();
-            let mut operation = 0;
-
-            while operands.len() > 1 {
-                let first = operands.pop_front().unwrap();
-                
-                if bit_map[operation] == 0 {
-                    operands[0] = operands[0] * first;
-                } else if bit_map[operation] == 1 {
-                    operands[0] = operands[0] + first;
-                }
-                operation += 1;
-            }
-            
-            // println!("{} {:?} {:?}", total, operands, bit_map);    
-            
-            if operands[0] == total {
-                // println!("Ans: {} {:?}", total, operands);                
-                answer += operands[0];
-                break;
-            } else {
-                let mut index = 0;
-                loop {
-                    bit_map[index] += 1;
-                    if bit_map[index] == 1 {
-                        break;
-                    } else if bit_map[index] == 2 {
-                        bit_map[index] = 0;
-                    }
-                    index += 1;
-                }
-            }
+        let total = split[0].parse::<u64>().unwrap();
+        let operands : VecDeque<u64> = split[1].trim().split(' ').map(|x| x.parse::<u64>().unwrap()).collect();
+        if is_valid(operands[0], total, &operands, 1) {
+            answer += total;
+            // println!("Valid {}", line);
+        } else {
+            // println!("Invalid {}", line);
         }
+        
+
+        // // let mut bit_map: Vec<i8> = vec![0; split[1].trim().split(' ').count()];
+
+        // // while bit_map.iter().map(|x| u64::from(*x as u16)).sum::<u64>() != (bit_map.len() * 2) as u64 {
+        // while *bit_map.last().unwrap() != 3 {
+        //     // println!("{}", bit_map.iter().map(|x| u64::from(*x as u16)).sum::<u64>());
+        //     let mut operands : VecDeque<u64> = split[1].trim().split(' ').map(|x| x.parse::<u64>().unwrap()).collect();
+        //     let mut operation = 0;
+        //     // println!("{:?}", bit_map);
+        //     while operands.len() > 1 {
+        //         let first = operands.pop_front().unwrap();
+                
+        //         if bit_map[operation] == 0 {
+        //             // println!("{} * {} = {}", first, operands[0], operands[0] * first);
+        //             operands[0] *= first;
+        //         } else if bit_map[operation] == 1 {
+        //             // println!("{} + {} = {}", first, operands[0], operands[0] + first);
+        //             operands[0] += first;
+        //         } else if bit_map[operation] == 2 {
+        //             let mut base : u64 = 1;
+        //             while operands[0] > base {
+        //                 base *= 10;
+        //             }
+
+        //             // println!("{} || {} = {}", first * base, operands[0], base * first + operands[0]);
+        //             operands[0] += first * base;
+        //         }
+
+        //         operation += 1;
+        //     }
+            
+            
+        //     if operands[0] == total {
+        //         // println!("Ans: {} {:?} {:?}", total, operands, bit_map);                
+        //         answer += operands[0];
+        //         break;
+        //     } else {
+        //         let mut index = 0;
+        //         loop {
+        //             bit_map[index] += 1;
+        //             if bit_map[index] != 3 {
+        //                 break;
+        //             } else if bit_map[index] == 3 && index < bit_map.len() - 1{
+        //                 bit_map[index] = 0;
+        //             }
+        //             index += 1;
+        //             if index == bit_map.len() {
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // if *bit_map.last().unwrap() == 2 {
+        //     // println!("Skipped: {} {:?}", line, bit_map);    
+        // }
     }
 
     return answer;
