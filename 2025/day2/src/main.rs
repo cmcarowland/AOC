445bd2df56d85a2b5cac82c1943195366f0c2ec8
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-12-01 13:44:10 +0000

    chore: Got day 2 configured for puzzle

---
+++ 2025/day2/src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please enter file path for data set");
+    }
+
+    pt1(&args[1]);
+    pt2(&args[1]);
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in fs::read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string());
+    }
+
+    result
+}
+
+fn pt1(filename : &str) {
+    let lines = read_lines(filename);
+    let answer : i64 = 0;
+
+    println!("PT1 : {}", answer);
+}
+
+fn pt2(filename : &str) {
+    let lines = read_lines(filename);
+    let answer : i64 = 0;
+
+    println!("PT2 : {}", answer);
+}
